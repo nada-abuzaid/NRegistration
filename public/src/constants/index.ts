3c0f46be16d7c8da1655f4120c8434539d67f9af
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2023-07-31 15:31:34 +0300

    fix: deploy settings

--- public/src/constants/index.ts
+++ public/src/constants/index.ts
@@ -1,4 +1,4 @@
 import { BUSINESS_INPUTS, INDIVIDUAL_INPUTS } from './inputs';
-import { BASE_URL, ENDPOINTS } from './endpoints';
+import { BASE_URL, ENDPOINTS, PREFIX } from './endpoints';
 
-export { BUSINESS_INPUTS, INDIVIDUAL_INPUTS, BASE_URL, ENDPOINTS };
+export { BUSINESS_INPUTS, INDIVIDUAL_INPUTS, BASE_URL, ENDPOINTS, PREFIX };
--- public/src/utils/api/axios.ts
+++ public/src/utils/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { BASE_URL } from '../../constants';
+import { PREFIX } from '../../constants';
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: PREFIX,
 });
 
 axiosInstance.interceptors.request.use(
