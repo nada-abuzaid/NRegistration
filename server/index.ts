7b510fa5214342a43e49b9b0d096095c66ae040d
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2023-07-31 15:21:24 +0300

    fix: deploy settings

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,27 @@
-import express from 'express';
+import express, { Request } from 'express';
 import cors from 'cors';
 import { countriesController, citiesController } from './controllers';
 import { clientError, serverError } from './middlewares';
 import { registerController } from './controllers/users';
+import { join } from 'path';
 
 const app = express();
 require('dotenv').config();
+console.log(process.env.NODE_ENV);
 
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/api/countries', countriesController);
 app.get('/api/cities/:country', citiesController);
 app.post('/api/user', registerController);
+
+if (process.env.NODE_ENV === 'production') {    
+  app.use(express.static(join(__dirname, '..', '..', 'public', 'dist')));
+  app.get('*', (_req, res) => {
+    res.sendFile(join(__dirname, '..', '..', 'public', 'dist', 'index.html'));
+  });
+}
 
 app.use(clientError);
 app.use(serverError);
